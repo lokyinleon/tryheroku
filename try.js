dc8fdf751340d8a99ca571ad715878c6ddbaf4b0
lokyinleon <31607348+lokyinleon@users.noreply.github.com>
2017-12-26 01:53:00 +0800

    Update try.js

--- try.js
+++ try.js
@@ -30,7 +30,7 @@ app.get('/greetings/sayHelloWithTime', function (req, res) {
   res.send(greetingMsg(req.query.name,true));
 });
 
-var server = app.listen(process.env.PORT || 8099, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var port = server.address().port;
   console.log('Server listening at port %s', port);
 });
