3a4fb803d92b90055617218f55897af28fb615b3
lokyinleon <31607348+lokyinleon@users.noreply.github.com>
2017-12-26 01:36:03 +0800

    Update try.js

--- try.js
+++ try.js
@@ -1,7 +1,36 @@
 var express = require('express');
 var app = express();
 
+function greetingMsg(name,showtime) {
+	var today = new Date();
+	var msg = (name != null) ? 'Hello ' + name + '! ' : 'Hello';
+	if (showtime) {
+		msg += " It is now " + today.toTimeString();
+	}
+	return(msg);
+}
 
-app.get('/', function(req, res) {
-  res.send('hello world');
+app.get('/', function (req, res) {
+
+res.redirect('/greetings');
+});
+
+app.get('/greetings', function (req, res) {
+  res.download('/Users/lokyinleon/Desktop/Node.js/ServerAPI2.js');
+  res.send(greetingMsg(req.query.name,false));
+  
+  console.log("****");
+});
+
+app.get('/greetings/sayHello', function (req, res) {
+  res.send(greetingMsg(req.query.name,false));
+});
+
+app.get('/greetings/sayHelloWithTime', function (req, res) {
+  res.send(greetingMsg(req.query.name,true));
+});
+
+var server = app.listen(process.env.PORT || 8099, function () {
+  var port = server.address().port;
+  console.log('Server listening at port %s', port);
 });
